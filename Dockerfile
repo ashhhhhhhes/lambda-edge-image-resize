6dd26a6703bed741c9d5de7165c46cdf45991581
hong ash <ash.hong@milkcorp.kr>
2023-11-29 23:02:53 +0900

    v18 버전 액세스 이슈로 노드 버전 다운그레이드

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 # al:2 에서 al:2023 버전으로 변경 (node v18 이슈)
 # (오류) node: /lib64/libm.so.6: version `GLIBC_2.27' not found (required by node)
-FROM amazonlinux:2023 
+FROM amazonlinux:2
 
 WORKDIR /tmp
 
@@ -15,6 +15,6 @@ RUN touch ~/.bashrc && chmod +x ~/.bashrc
 
 RUN curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-RUN source ~/.bashrc && nvm install 18.12.1
+RUN source ~/.bashrc && nvm install 14.15.1
 
 WORKDIR /build
--- src/index_v18.js
+++ src/index_v18.js
@@ -1,14 +1,16 @@
 "use strict";
 
+// ❶ 필수 모듈
 const querystring = require("querystring"); // Don't install.
-const AWS = require("aws-sdk"); // Don't install.
+const { S3Client, GetObjectAclCommand } = require("@aws-sdk/client-s3"); // aws-sdk v3로 변경 (Dont' install.)
 const Sharp = require("sharp");
 
-const S3 = new AWS.S3({
+// ❷ S3 클라이언트, 버킷명 설정
+const client = new S3Client({
   region: "ap-northeast-2",
 });
 
-// TODO 버켓 설정 필요
+// ❷-1. 버킷명 버킷명이 틀리면 오류가 발생할 수 있습니다.
 const BUCKET = "lo-gos-test";
 
 /**
@@ -29,6 +31,12 @@ const isGif = (format) => {
   return format === "gif";
 };
 
+/**
+ * 클라이언트에 반환할 이미지 포맷 생성 함수
+ * @param {*} changeFormat 변경할 이미지포맷
+ * @param {*} extension 현재 파일 확장자명
+ * @returns
+ */
 function initFormat(changeFormat, extension) {
   let result = changeFormat || extension;
 
@@ -41,10 +49,22 @@ function initFormat(changeFormat, extension) {
   return result;
 }
 
+/**
+ * 이미지 높이 세팅 함수
+ * @param {*} metaHeight 원본 이미지 높이
+ * @param {*} paramsHeight 변경할 이미지 높이
+ * @returns
+ */
 function initHeight(metaHeight, paramsHeight) {
   return paramsHeight ? Math.min(metaHeight, parseInt(paramsHeight, 10)) : null;
 }
 
+/**
+ * 이미지 넓이 세팅 함수
+ * @param {*} metaWidth 원본 이미지 높이
+ * @param {*} paramsWidth 변경할 이미지 넓이
+ * @returns
+ */
 function initWidth(metaWidth, paramsWidth) {
   return paramsWidth ? Math.min(metaWidth, parseInt(paramsWidth, 10)) : null;
 }
@@ -76,13 +96,9 @@ exports.handler = async (event, context, callback) => {
 
   // Extract name and format.
   const { uri } = request;
-  let [, imageName, extension] = uri.match(/\/?(.*)\.(.*)/);
-
-  extension = extension.toLowerCase();
+  const [, imageName, extension] = uri.match(/\/?(.*)\.(.*)/);
 
   if (isGif(extension)) {
-    console.log("GIF image requested!");
-    console.log("response, content-type", response.headers["content-type"]);
     // change content-type to image/gif
     response.headers["content-type"] = [
       {
@@ -100,22 +116,30 @@ exports.handler = async (event, context, callback) => {
   let resizedImage;
 
   try {
-    s3Object = await S3.getObject({
+    console.log(
+      "Bucket !! :::",
+      BUCKET,
+      decodeURI(imageName + "." + extension)
+    );
+    const command = await new GetObjectAclCommand({
       Bucket: BUCKET,
       Key: decodeURI(imageName + "." + extension),
-    }).promise();
+    });
+
+    s3Object = await client.send(command);
   } catch (error) {
-    console.log("S3.getObject: ", error);
+    console.error("S3.getObject: ", error);
     return callback(error);
   }
 
   const origintLength = s3Object.ContentLength;
-  console.log(`origin byteLength ${origintLength}`);
+
+  console.log(origintLength, "origintLength");
 
   try {
     const image = Sharp(s3Object.Body, {
       animated: isGif(extension),
-      // failOn: "truncated", // 짤린(손상된) 이미지만 오류 반환
+      // failOn: "truncated", // 짤린(손상된) 이미지 오류 반환
       failOn: "none", // 모든 변환 실패 오류 반환 하지 않음
     });
 
@@ -131,6 +155,8 @@ exports.handler = async (event, context, callback) => {
     console.log(`name: ${imageName}.${extension}`); // Favicon error, if name is `favicon.ico`.
 
     // Image crop logic
+    // crop={width}x{height}
+    // crop=100x100 = [100, 100]
     const crop = params.crop ? params.crop.split("x") : null;
 
     // crop
@@ -173,7 +199,7 @@ exports.handler = async (event, context, callback) => {
 
     resizedImage = await image.toBuffer();
   } catch (error) {
-    console.log("Sharp: ", error);
+    console.error("Sharp: ", error);
     return callback(error);
   }
 
@@ -196,9 +222,9 @@ exports.handler = async (event, context, callback) => {
     },
   ];
 
-  // cahce
+  // cahce 만료 시간 설정
   response.headers["cache-control"] = [
-    { key: "Cache-Control", value: "public, max-age=5184000" },
+    { key: "Cache-Control", value: "public, max-age=86400" },
   ];
 
   return callback(null, response);
